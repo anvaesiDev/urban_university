0e860dd1094f67b60b4387717c9a1f037367ebd6
anvaesiDev <anvaesi.dev@gmail.com>
2024-07-02 13:43:37 +0300

    Homework 2 variables

---
+++ homework2.py
@@ -0,0 +1,5 @@
+num_of_solved_tasks = 12
+spended_time = 1.5
+course_name = "Python"
+time_per_task = spended_time/num_of_solved_tasks
+print(f"Курс: {course_name}, всего задач:{num_of_solved_tasks}, затрачено часов: {spended_time}, среднее время выполнения {time_per_task} часа.")
